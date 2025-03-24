e3aef360d8e2dc107bf75bf85e4d01faae92946a
Dennis515jd <dennis515jd@gmail.com>
2025-03-24 14:13:56 +0800

    save

--- Scene/GameMain/PlayerSlime/RigidBall/rigid_ball.gd
+++ Scene/GameMain/PlayerSlime/RigidBall/rigid_ball.gd
@@ -1,10 +1,21 @@
 extends RigidBody2D
 
+const MAX_DISTANCE := 10.0
+var front:RigidBody2D
+var back:RigidBody2D
+
+#func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	#var pos = state.transform.origin
+	#if (front.position - pos).length()>= MAX_DISTANCE:
+		#pos = _vec2_resize(front.position, pos, MAX_DISTANCE/2.0)
+	#if (back.position - pos).length()>= MAX_DISTANCE:
+		#pos = _vec2_resize(back.position, pos, MAX_DISTANCE/2.0)
+	#state.transform.origin = pos
+	#reset_physics_interpolation.call_deferred()
+	##position = pos
+
+func _vec2_resize(pos:Vector2, target:Vector2, new_length:float)-> Vector2:
+	return (target - pos).normalized()*new_length + pos
 
 
-
-
-#func _physics_process(delta: float) -> void:
-	#if (get_global_mouse_position() - position).length() <= 10.0 and \
-		#Input.is_action_pressed("L_click"):
-		#apply_central_impulse((get_global_mouse_position() - position) * 300.0 * delta)
+#
--- Scene/GameMain/PlayerSlime/slime_body.gd
+++ Scene/GameMain/PlayerSlime/slime_body.gd
@@ -3,15 +3,16 @@ extends Node
 func _ready() -> void:
 	_spawn_body()
 func _physics_process(delta: float) -> void:
+	#_test_move()
+	#_distance_constraint()
+	#_area_constraint()
 	_test_move()
-	_distance_constraint()
-	_area_constraint()
-	_test_move()
+	_set_camera()
 
 const BODY_POINT_SIZE = 30
 const BODY_POINT_R = 50.0
 
-var _body_point_arr: Array[RigidBody2D] = []
+var _body_point_arr: Array = []
 
 func _spawn_body():
 	for i in range(BODY_POINT_SIZE):
@@ -21,35 +22,50 @@ func _spawn_body():
 		node.position = Vector2( sin(angle), cos(angle) ) * BODY_POINT_R
 		_body_point_arr.append( node )
 		add_child( node )
-		node.linear_damp = DAMPING
+		#node.linear_damp = DAMPING
+	
+	for i in range(BODY_POINT_SIZE):
+		_body_point_arr[i].front = _body_point_arr[(i+1) % BODY_POINT_SIZE]
+		_body_point_arr[i].back = _body_point_arr[(i-1)]
+		
+	for i in range(BODY_POINT_SIZE):
+		var join = preload("uid://der36220txty8").instantiate()
+		
+		join.node_a = _body_point_arr[i].get_path()
+		join.node_b = _body_point_arr[(i+1) % BODY_POINT_SIZE].get_path()
+		print(join.node_a, join.node_b)
+		add_child(join)
+
+
+
 
 const FIX_SPEED: = 40.0
 const MAX_DISTANCE := 5.0
 const DAMPING := 1.5
 #const G = 5.0
+#
+#func _distance_constraint():
+	#for i in range(BODY_POINT_SIZE):
+		#var body_a = _body_point_arr[i]
+		#var body_b = _body_point_arr[(i+1) % BODY_POINT_SIZE]
+		#
+		## 计算当前两刚体之间的距离差
+		#var delta = body_b.position - body_a.position
+		#var current_distance = delta.length()
+		#var error = current_distance - MAX_DISTANCE
+		#
+		## 如果当前距离超过允许的最大距离，则施加修正冲量
+		#if error > 0:
+			## 修正量取一半分别施加给 body_a 和 body_b，确保整体平衡
+			#var correction = delta.normalized() * error * 0.5 * FIX_SPEED
+			##body_a.velocity += correction
+			##body_b.velocity += -correction
+			#body_a.apply_central_impulse(correction)
+			#body_b.apply_central_impulse(-correction)
 
-func _distance_constraint():
-	for i in range(BODY_POINT_SIZE):
-		var body_a = _body_point_arr[i]
-		var body_b = _body_point_arr[(i+1) % BODY_POINT_SIZE]
-		
-		# 计算当前两刚体之间的距离差
-		var delta = body_b.position - body_a.position
-		var current_distance = delta.length()
-		var error = current_distance - MAX_DISTANCE
-		
-		# 如果当前距离超过允许的最大距离，则施加修正冲量
-		if error > 0:
-			# 修正量取一半分别施加给 body_a 和 body_b，确保整体平衡
-			var correction = delta.normalized() * error * 0.5 * FIX_SPEED
-			#body_a.velocity += correction
-			#body_b.velocity += -correction
-			body_a.apply_central_impulse(correction)
-			body_b.apply_central_impulse(-correction)
 
-
-const AREA = 0.1
-const MAX_CORRECTION_STRENGTH = 100.0  # 限制最大修正力度
+const AREA = 10.0
+const MAX_CORRECTION_STRENGTH = 30.0  # 限制最大修正力度
 
 func _area_constraint():
 	var polygon: PackedVector2Array = []
@@ -115,7 +131,13 @@ func _test_move():
 		#_body_point_arr[0].position = _body_point_arr[0].get_global_mouse_position()
 
 
-
+func _set_camera():
+	var center_pos = Vector2.ZERO
+	for i in _body_point_arr:
+		center_pos += i.position
+	center_pos /=BODY_POINT_SIZE
+	%Camera2D.offset = center_pos
+	#%Camera2D.offset = _body_point_arr[0].position
 
 
 
